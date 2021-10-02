41686ac66d699a1f59daef9b4c47f42b42a51661
me <me@me.me.com>
2021-10-02 08:57:23 -0700

    fix error with updating to users in drag and drop events.

--- Data/TeamsUpdateService.cs
+++ Data/TeamsUpdateService.cs
@@ -1,15 +1,33 @@
 ﻿using System;
+using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using BlazorMeetup.Pages;
+using System.Diagnostics;
 namespace BlazorMeetup.Data
 {
     public class TeamsUpdateService
     {
+        public void RemoveDelegate(string eventId,Func<Task> myDelegate)
+        {
+            UpdateEvents[eventId] -= myDelegate;
+        }
         public void InvokeUpdate(string eventId)
         {
-            UpdateEvents[eventId]?.Invoke();
+            Debug.WriteLine(UpdateEvents[eventId].GetInvocationList().Length.ToString() + " number of delegates");
+           foreach(var del in UpdateEvents[eventId].GetInvocationList())
+            {
+                try
+                {
+                    del.DynamicInvoke();
+                }
+                catch (Exception xx)
+                {
+                    Console.WriteLine(string.Format("Exception in {0}: {1}", del.Method.Name, xx.Message));
+                }
+            //https://csharp.2000things.com/tag/getinvocationlist/
+            }
         }
 
         public void AddEvent(string eventId,Func<Task> newDelegate)
