b73168501d199601605613461eace7f02dc2ebe3
me <me@me.me.com>
2021-10-31 11:39:22 -0700

    fix issue with caching by creating original names

--- Data/AvatarSettings.cs
+++ Data/AvatarSettings.cs
@@ -9,6 +9,7 @@ namespace BlazorMeetup.Data
     {
         public string Id { get; set; }
         public string AvatarUrl { get; set; }
+        public string AvatarIdentification { get; set; }
         public string AttendeeId { get; set; }
         public int Size { get; set; } = 100;
         public int Left { get; set; } = 0;
---
+++ Migrations/20211031182022_avatardiscriminator.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlazorMeetup.Migrations
+{
+    public partial class avatardiscriminator : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AvatarIdentification",
+                table: "AvatarSettings",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AvatarIdentification",
+                table: "AvatarSettings");
+        }
+    }
+}
