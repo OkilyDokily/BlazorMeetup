85c6a78959fdc91c481d61494c77cce179c41639
me <me@me.me.com>
2021-11-16 14:07:35 -0800

    show success when user edits avatar

--- Data/IJSRunTimeExtension.cs
+++ Data/IJSRunTimeExtension.cs
@@ -1,16 +1,12 @@
 ﻿using Microsoft.JSInterop;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace BlazorMeetup.Data
 {
     public static class IJSRunTimeExtension
     {
-        public static async void CreateMessage(this IJSRuntime jSRuntime,string message)
+        public static async void CreateMessage(this IJSRuntime jSRuntime, string message)
         {
-            await jSRuntime.InvokeVoidAsync("callToastr",message);
+            await jSRuntime.InvokeVoidAsync("callToastr", message);
         }
     }
 }
