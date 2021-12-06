87eb81cc1bf525e175cfddebf2cd1f2ccb5cb528
me <me@me.me.com>
2021-12-06 00:15:45 -0800

    fix problem with suggest times showing blank

--- Data/TimeMaker.cs
+++ Data/TimeMaker.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Diagnostics;
-using System.Threading.Tasks;
 
 namespace BlazorMeetup.Data
 {
@@ -12,56 +10,74 @@ namespace BlazorMeetup.Data
         public DateTime Date { get; set; } = DateTime.Today;
         public int Minutes { get; set; }
 
-        public DateTime MakeTime()
-        {            
-           DateTime am = Date.AddMinutes(Minutes);
-           DateTime ah = am.AddHours(Hours);
-           return ah;
+        public TimeMaker()
+        {
+
         }
 
-        public List<int> GetHours(List<TimesAllowed> ta = null)
+        public TimeMaker(List<int> hours)
         {
-            if(ta == null || ta.Count == 0)
-            return Enumerable.Range(0, 24).ToList();
 
-            List<int> er = new ();
-          
+            Hours = hours.First();
+
+        }
+
+        public TimeMaker(List<int> hours, List<int> minutes)
+        {
+
+            Hours = hours.First();
+            Minutes = minutes.First();
+        }
+        public DateTime MakeTime()
+        {
+            DateTime am = Date.AddMinutes(Minutes);
+            DateTime ah = am.AddHours(Hours);
+            return ah;
+        }
+
+        public static List<int> GetHours(List<TimesAllowed> ta = null)
+        {
+            if (ta == null || ta.Count == 0)
+                return Enumerable.Range(0, 24).ToList();
+
+            List<int> er = new();
+
             foreach (TimesAllowed t in ta)
             {
                 List<int> rfer = Enumerable.Range(t.BeginningHour, (t.EndingHour - t.BeginningHour) + 1).ToList();
-            
-                foreach(int r in rfer)
-                {                 
+
+                foreach (int r in rfer)
+                {
                     if (er.Any(x => x == r))
                     {
                         continue;
                     }
                     er.Add(r);
-                }         
+                }
             }
-            return er;      
+            return er;
         }
-         
-        public List<int> GetMinutes(int hour = -1,List<TimesAllowed> ta = null)
+
+        public static List<int> GetMinutes(int hour = -1, List<TimesAllowed> ta = null)
         {
-            if(ta == null || hour == -1 || ta.Count == 0)
-            return new List<int>(){0, 15,30,45};
-            List<int> mr = new List<int>() {0, 15, 30, 45 };
+            if (ta == null || hour == -1 || ta.Count == 0)
+                return new List<int>() { 0, 15, 30, 45 };
+            List<int> mr = new List<int>() { 0, 15, 30, 45 };
             foreach (TimesAllowed t in ta)
             {
                 List<int> rfer = Enumerable.Range(t.BeginningHour, (t.EndingHour - t.BeginningHour) + 1).ToList();
-                if(rfer.Any(x=> x == hour))
+                if (rfer.Any(x => x == hour))
                 {
-                    if(!(rfer.First() == hour) && !(rfer.Last() == hour))
+                    if (!(rfer.First() == hour) && !(rfer.Last() == hour))
                     {
-                       return new List<int>() {0, 15, 30, 45 };
+                        return new List<int>() { 0, 15, 30, 45 };
                     }
 
-                    if((rfer.First() == hour) && (rfer.Last() == hour))
+                    if ((rfer.First() == hour) && (rfer.Last() == hour))
                     {
-                        return mr.Where(x => x >= t.BeginningMinute  && x < t.EndingMinute).ToList();
+                        return mr.Where(x => x >= t.BeginningMinute && x < t.EndingMinute).ToList();
                     }
-                    if(rfer.First() == hour)
+                    if (rfer.First() == hour)
                     {
                         return mr.Where(x => x >= t.BeginningMinute).ToList();
                     }
