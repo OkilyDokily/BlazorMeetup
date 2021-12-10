8ccb248999c7600681c89f995bd5139199e1353f
me <me@me.me.com>
2021-12-10 15:12:37 -0800

    made comment

--- Areas/Identity/Data/BlazorMeetupContext.cs
+++ Areas/Identity/Data/BlazorMeetupContext.cs
@@ -23,7 +23,7 @@ namespace BlazorMeetup.Data
         public virtual DbSet<TeamAttendee> TeamAttendees { get; set; }
         public virtual DbSet<AvatarSettings> AvatarSettings { get; set; }
         public virtual DbSet<TeamAvatarSettings> TeamAvatarSettings { get; set; }
-
+        //new update
         // protected override void OnModelCreating(ModelBuilder builder)
         // {
         //     builder.Entity<SuggestedDate>().HasOne(x => x.RestrictDate).WithMany(x => x.SuggestedDates).OnDelete(DeleteBehavior.Cascade);
